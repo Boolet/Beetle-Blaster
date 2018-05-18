691459d3faa16ac24f1ec08d2102a6e65656cc4d
Avi Miller <TrebecS@Gmail.com>
2018-05-18 13:52:54 -0700

    Did some cleaning and added debris-knockoff to the ball

---
+++ Assets/Scripts/Ball/DebrisBallCollisionControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DebrisBall))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class DebrisBallCollisionControl : MonoBehaviour {
+
+	[SerializeField] DebrisBall ball;
+	[SerializeField] float forceThreshold = 0.5f;
+	[SerializeField] float forcePerDebris = 0.7f;
+	[SerializeField] float knockOffImpulseFactor = 0.2f;
+	[SerializeField] int maxContactPoints = 4;
+
+	void OnCollisionEnter2D(Collision2D col){
+		//this doesn't handle the cases of the other body being kinematic, does it?
+		float greatestCollisionForce = 0f;
+		Vector2 contactNormal = Vector2.zero;
+
+		ContactPoint2D[] contacts = new ContactPoint2D[maxContactPoints];
+		col.GetContacts(contacts);
+		foreach (ContactPoint2D cp in contacts){
+			if (cp.normalImpulse > greatestCollisionForce){
+				greatestCollisionForce = cp.normalImpulse;
+				contactNormal = cp.normal;
+			}
+		}
+
+		if (greatestCollisionForce > forceThreshold){
+			BreakOffBall(greatestCollisionForce, contactNormal);
+			return;
+		} else{
+			AddToBall(col.gameObject);
+		}
+	}
+
+	/// <summary>
+	/// causes debris to break off the ball when the ball is hit hard enough.
+	/// 
+	/// It should really produce a random amount of debris instead, taking into account the impact force
+	/// </summary>
+	/// <param name="impactMagnitude">Impact magnitude.</param>
+	/// <param name="collisionNormal">Collision normal.</param>
+	void BreakOffBall(float impactMagnitude, Vector2 collisionNormal){
+		int damage = Mathf.FloorToInt(impactMagnitude / forcePerDebris);
+		Debris[] spawnedDebris = ball.SpawnDebrisFromBall(damage);
+		foreach (Debris d in spawnedDebris){
+			d.rigidBody2D.AddForce(-collisionNormal * knockOffImpulseFactor, ForceMode2D.Impulse);
+		}
+	}
+
+	void AddToBall(GameObject obj){
+		Debris otherDebris = obj.GetComponent<Debris>();
+		if (otherDebris != null){
+
+			//may want to negate or mitigate velocity gained from absorbed garbage
+			//or maybe not!
+
+			ball.CollapseDebrisToBall(otherDebris);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Camera/CameraTracking.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraTracking : MonoBehaviour {
+
+	[SerializeField] Transform cameraTarget;
+
+	float cameraDistance = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LockOn();
+	}
+
+	void LockOn(){
+		transform.position = cameraTarget.position - Vector3.forward * cameraDistance;
+	}
+}
