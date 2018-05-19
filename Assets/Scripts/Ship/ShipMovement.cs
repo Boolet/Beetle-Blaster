9f3951029fe7fdad62dee20b9aac6fabd26a0b8b
Grant Hirahara <ghirahar@uci.edu>
2018-05-19 12:37:06 -0700

    Added Script for Ship Movement
    Currently references the ShipThrusters script and calls the ThrustInDirection(Vector2 direction) function based on the player's input

---
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+    public ShipThrusters shipThrusters;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		if(Input.GetKey(KeyCode.W))
+        {
+            shipThrusters.ThrustInDirection(Vector2.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            shipThrusters.ThrustInDirection(Vector2.left);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            shipThrusters.ThrustInDirection(Vector2.down);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            shipThrusters.ThrustInDirection(Vector2.right);
+        }
+    }
+}
